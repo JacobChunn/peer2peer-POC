85c04e1e7a5ce3faffca4c6f4c36a27c7c4cf9cf
Jake Chunn <35715914+reallylogical@users.noreply.github.com>
2022-06-12 16:27:29 -0400

    Changed peer2peer-games to peer2peer-POC
    After learning about security risks and disadvantages of using peerJS to implement a game room using peer2peer connection, I decided to transform this project into a proof of concept that peerJS is able to connect users together. This way, I can look elsewhere for a safe way to create a game room, and learn more about other networking technologies.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# peer2peer-games
- A website that connects you to a friend directly to play games. Created using PeerJS
+# peer2peer-POC
+ A proof of concept, showing peer to peer connection between two users.
+ Developed for all screen sizes.
+ Created with peerJS.
+
+ HOW TO USE:
+
+1.) Set up a testing server loaded with this project
+2.) Have two separate users (can be on the same machine) connect to the server
+3.) One user must get their peer ID via the copy link button
+4.) The other user must enter their ID into the input box and submit
+5.) Observe the connection in the console of both users
--- js/peerConnections.js
+++ js/peerConnections.js
@@ -1,30 +1,39 @@
+/*
+A script that allows a peer to connect to another peer by generating a
+peer ID, or by accepting a peer ID to connect to. Created with PeerJS
+Made by Jacob Chunn
+*/
+
+// Create peer object to connect to
 var peer = new Peer();
 
-var joinGameButton = document.querySelector("#joinGameButton");
-var joinGameInput = document.querySelector("#joinGameInput");
+// Get both the join button and input
+var joinButton = document.querySelector("#joinButton");
+var joinInput = document.querySelector("#joinInput");
 
+// On peer start, get ID of self and dictate rules of
+// what happens when connected to another peer
 peer.on('open', function(id) {
 	console.log('My peer ID is: ' + id);
-	peerURL = createPeerURL(peer.id);
-	console.log(peerURL);
 
-	$("#copyInviteLink").click(function() {
-		navigator.clipboard.writeText(createPeerURL(peer.id));
-	});
-
+	// Copy ID of self to clipboard when Copy ID button is clicked
 	$("#copyInviteID").click(function() {
 		navigator.clipboard.writeText(peer.id);
 	});
 
+	// Dictate rules of peer connection and disconnection
 	peer.on("connection", function(conn) {
 		conn.on("open", function() {
 
+			// When data is recieved, log the data
 			conn.on("data", function(data) {
 				console.log("Received data: " + data);
 			});
-		
+			
+			// On peer connected, send a string of data
 			conn.send("First sent data!");
 
+			// Log when peer connection is closed
 			conn.on("close", function() {
 				console.log("Peer connection closed")
 			})
@@ -34,37 +43,26 @@ peer.on('open', function(id) {
 
   });
 
-joinGameButton.addEventListener("click", function() {
-	console.log("Join Game ID: " + joinGameInput.value)
-	conn = peer.connect(joinGameInput.value);
+// Connect to provided peer ID when join button is clicked and
+// dictate rules of connection and disconnection
+joinButton.addEventListener("click", function() {
+	console.log("Join with Peer ID: " + joinInput.value)
+	conn = peer.connect(joinInput.value);
 
+	// Dictate rules of peer connection and disconnection
 	conn.on("open", function() {
 
+		// When data is recieved, log the data
 		conn.on("data", function(data) {
 			console.log("Received data: " + data);
 		});
-	
+
+		// On peer connected, send a string of data
 		conn.send("First sent data!");
-	
+		
+		// Log when peer connection is closed
 		conn.on("close", function() {
 			console.log("Peer connection closed")
 		})
 	})
 })
-
-
-function createPeerURL(peerID) {
-	const URL = window.location.host;
-	const PROTOCOL = window.location.protocol;
-	return "" + PROTOCOL + "//" + URL + "/?id=" + peerID;
-}
-
-function recieveConnection() {
-	peer.on("connection", function(conn) {
-		return conn;
-	});
-}
-
-function copyInviteID() {
-
-}
